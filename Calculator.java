e0806494f2154f4ffa93c3ae0ae19ac57a2a5886
Marlin Roberts <marlinroberts@u.boisestate.edu>
2019-06-20 11:54:18 -0600

    Added basic GUI code for panel layout

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,7 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
 /*
  * @author TEAM BeardedGlory
  * 
@@ -11,7 +15,34 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
+		GUI();
 	}
+	
+	/**
+	 * GUI creates a new JFrame object, 
+	 * creates an instance of the inner class GUI and
+	 * displays it 
+	 */
+	private static void GUI() {
+		
+		// new JFrame with exit option
+		JFrame frame = new JFrame("Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Create and add main panel
+		JPanel mainPanel = new JPanel();
+		frame.getContentPane().add(mainPanel);
+		
+		// Fill out the GUI here....
+		
+		
+		
+		
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+	} // end startGUI
+	
 
 }
