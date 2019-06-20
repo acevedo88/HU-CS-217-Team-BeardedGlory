8f697202a66a232fbaf9f0943c963e376aeac893
alexacevedo <alexacevedo@boisestate.edu>
2019-06-20 10:21:42 -0600

    Add Javadoc to beginning

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,11 @@
-
+/*
+ * @author Alex Acevedo TEAM BeardedGlory
+ * 
+ * 
+ * The Calculator class is the main class that will instantiate the GUI and 
+ * calculate class for user interactions.
+ * 
+ */
 public class Calculator {
 
 	public static void main(String[] args) {
