0d59b59ed5c0a028d66d94dc92d8cd6c56ddef73
williamallen136 <williamallen136@u.boisestate.edu>
2019-06-27 12:13:53 -0600

    [Issue #46] Correct Output from Subtraction Command
    	Correction to order in which variables were subtracted by swapping order.

--- Calculator.java
+++ Calculator.java
@@ -182,7 +182,7 @@ public class Calculator {
 			
 			else if(operation == "-")
 			{
-				resultObtained = resultObtained + (tmp1-tmp2);
+				resultObtained = resultObtained + (tmp2-tmp1);
 			}
 			
 			else if(operation == "*")
